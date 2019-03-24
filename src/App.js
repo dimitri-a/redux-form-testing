1703ecede21d10577f4d3347780e84eea44580de
Dimitri C <couwenberg@gmail.com>
2019-03-24 15:57:00 +1100

    why does name component not work?

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React, { Component } from 'react';
+// import './App.css';
+// import SimpleForm from './SimpleForm'
+// import showResults from './showResults'
+// import { Values } from "redux-form-website-template";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+// class App extends Component {
+//   render() {
+//     return (
+//       <div style={{ padding: 15 }}>
+//       <h2>Simple Form</h2>
+//       <SimpleForm onSubmit={showResults} />
+//       <Values form="simple" />
+//     </div>
+//     );
+//   }
+// }
 
-export default App;
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { Values } from "redux-form-website-template";
+import store from "./store";
+import showResults from "./showResults";
+import SimpleForm from "./SimpleForm";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootEl = document.getElementById("root");
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <Provider store={store}>
+    <div style={{ padding: 15 }}>
+      <h2>Simple Form</h2>
+      <SimpleForm onSubmit={showResults} />
+      <Values form="simple" />
+    </div>
+  </Provider>,
+  rootEl
+);
